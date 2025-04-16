c5f74ba324c0c64dc248bea8416b006674c6ae03
HamadMehmood <hamad.mehmood112000@gmail.com>
2025-04-17 02:33:30 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const messageEl = document.getElementById("registerMessage");
 
       try {
-        const res = await fetch("http://localhost:5000/api/auth/register", {
+        const res = await fetch("https://metacomart-2.onrender.com", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ username, password })
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const messageEl = document.getElementById("loginMessage");
 
       try {
-        const res = await fetch("http://localhost:5000/api/auth/login", {
+        const res = await fetch("https://metacomart-2.onrender.com", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ username, password })
